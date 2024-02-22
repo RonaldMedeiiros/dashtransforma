7637923bc3bb751c99ba62fdb82c7de09204f7e0
Ronaldo Medeiros <ronaldmedeiiros@gmail.com>
2024-02-22 16:01:44 -0400

    Tradução perguntas survey

--- survey/survey.js
+++ survey/survey.js
@@ -2,26 +2,26 @@ const surveyJson = {
     pages: [{
         elements: [{
             name: "satisfaction-score",
-            title: "How would you describe your experience with our product?",
+            title: "Como você descreve a sua satisfação com nosso produto?",
             type: "radiogroup",
             choices: [
-                { value: 5, text: "Fully satisfying" },
-                { value: 4, text: "Generally satisfying" },
-                { value: 3, text: "Neutral" },
-                { value: 2, text: "Rather unsatisfying" },
-                { value: 1, text: "Not satisfying at all" }
+                { value: 5, text: "Totalmente satisfeito" },
+                { value: 4, text: "Satisfeito" },
+                { value: 3, text: "Neutro" },
+                { value: 2, text: "Insatisfeito" },
+                { value: 1, text: "Totalmente insatisfeito" }
             ],
             isRequired: true
         }]
     }, {
         elements: [{
             name: "what-would-make-you-more-satisfied",
-            title: "What can we do to make your experience more satisfying?",
+            title: "O que podemos fazer para tornar sua experiência mais satisfatória?",
             type: "comment",
             visibleIf: "{satisfaction-score} = 4"
         }, {
             name: "nps-score",
-            title: "On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?",
+            title: "Em uma escala de zero a dez, qual é a probabilidade de você recomendar nosso produto a um amigo ou colega?",
             type: "rating",
             rateMin: 0,
             rateMax: 10
@@ -30,14 +30,14 @@ const surveyJson = {
     }, {
         elements: [{
             name: "how-can-we-improve",
-            title: "In your opinion, how could we improve our product?",
+            title: "Na sua opinião, como poderíamos melhorar nosso produto?",
             type: "comment"
         }],
         visibleIf: "{satisfaction-score} = 3"
     }, {
         elements: [{
             name: "disappointing-experience",
-            title: "Please let us know why you had such a disappointing experience with our product",
+            title: "Informe-nos por que você teve uma experiência tão decepcionante com nosso produto",
             type: "comment"
         }],
         visibleIf: "{satisfaction-score} =< 2"
